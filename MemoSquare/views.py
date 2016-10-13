21b26cba7cf3787d33da10654085ecba5aee36a3
student777 <darkdakku@gmail.com>
2016-10-13 16:53:31 +0900

    django REST mixins

--- MemoSquare/views.py
+++ MemoSquare/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from django.http import Http404
 from .models import Memo
 from .serializers import MemoSerializer
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 
 def index(request):
@@ -18,47 +16,27 @@ def logout_view(request):
 
 
 # REST framework practice
-class MemoList(APIView):
-    """
-    List all memos, or create a new memo.
-    """
-    def get(self, request, format=None):
-        memos = Memo.objects.all()
-        serializer = MemoSerializer(memos, many=True)
-        return Response(serializer.data)
+class MemoList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Memo.objects.all()
+    serializer_class = MemoSerializer
 
-    def post(self, request, format=None):
-        serializer = MemoSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class MemoDetail(APIView):
-    """
-    Retrieve, update or delete a memo instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Memo.objects.get(pk=pk)
-        except Memo.DoesNotExist:
-            raise Http404
+class MemoDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Memo.objects.all()
+    serializer_class = MemoSerializer
 
-    def get(self, request, pk, format=None):
-        memo = self.get_object(pk)
-        serializer = MemoSerializer(memo)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        memo = self.get_object(pk)
-        serializer = MemoSerializer(memo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, pk, format=None):
-        memo = self.get_object(pk)
-        memo.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
