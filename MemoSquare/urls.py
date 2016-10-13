f1757973e496151f822ca0c000a0d835ca9e6e42
student777 <darkdakku@gmail.com>
2016-10-13 16:42:37 +0900

    django REST class-based views

--- MemoSquare/urls.py
+++ MemoSquare/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from MemoSquare import views
+from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 
@@ -10,8 +10,8 @@ urlpatterns = [
     url(r'^logout/', views.logout_view, name="logout"),
     # REST practice
     # url(r'^api/', include('MemoSquare.urls-api')),
-    url(r'^memo/$', views.memo_list),
-    url(r'^memo/(?P<pk>[0-9]+)/$', views.memo_detail),
+    url(r'^memo/$', views.MemoList.as_view()),
+    url(r'^memo/(?P<pk>[0-9]+)/$', views.MemoDetail.as_view()),
     url(r'^', views.index, name="index"),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- MemoSquare/views.py
+++ MemoSquare/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from rest_framework.parsers import JSONParser
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from MemoSquare.serializers import MemoSerializer
+from django.http import Http404
 from .models import Memo
+from .serializers import MemoSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 def index(request):
@@ -18,46 +18,47 @@ def logout_view(request):
 
 
 # REST framework practice
-@api_view(['GET', 'POST'])
-def memo_list(request):
+class MemoList(APIView):
     """
-    List all code memos, or create a new memo.
+    List all memos, or create a new memo.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         memos = Memo.objects.all()
         serializer = MemoSerializer(memos, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MemoSerializer(data=data)
+    def post(self, request, format=None):
+        serializer = MemoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def memo_detail(request, pk):
+class MemoDetail(APIView):
     """
     Retrieve, update or delete a memo instance.
     """
-    try:
-        memo = Memo.objects.get(pk=pk)
-    except Memo.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return Memo.objects.get(pk=pk)
+        except Memo.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        memo = self.get_object(pk)
         serializer = MemoSerializer(memo)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        memo = self.get_object(pk)
         serializer = MemoSerializer(memo, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        memo = self.get_object(pk)
         memo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
