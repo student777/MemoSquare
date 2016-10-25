d3c77fa945f1e1a1e7b5adf22e4488cacdf65ac7
student777 <darkdakku@gmail.com>
2016-10-25 09:40:00 +0900

    Edit views_api.py: get memo by user

--- MemoSquare/views_api.py
+++ MemoSquare/views_api.py
@@ -2,7 +2,7 @@ from .models import Memo, Page
 from .serializers import MemoSerializer, UserSerializer, PageSerializer
 from rest_framework import viewsets
 from rest_framework import permissions
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, list_route
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
@@ -10,22 +10,27 @@ from .permissions import IsOwnerOrReadOnly
 
 
 class MemoViewSet(viewsets.ModelViewSet):
-    """
-    This viewset automatically provides `list`, `create`, `retrieve`,
-    `update` and `destroy` actions.
-    """
     queryset = Memo.objects.all()
     serializer_class = MemoSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly,)
 
+    class Meta:
+        model = Memo
+        fields = ('url', 'pk', 'content', 'owner', 'page',
+                  'clipper', 'is_private', 'timestamp')
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user, page=self.request.page)
 
+    @list_route(url_path='user')
+    def get_memo_of_owner(self, request):
+        memo_list = Memo.objects.filter(owner__id=request.user.id)
+        if memo_list is not None:
+            serializer = self.get_serializer(memo_list, many=True)
+            return Response(serializer.data)
+
 
 class PageViewSet(viewsets.ReadOnlyModelViewSet):
-    """
-    This viewset automatically provides `list` and `detail` actions.
-    """
     queryset = Page.objects.all()
     serializer_class = PageSerializer
 
