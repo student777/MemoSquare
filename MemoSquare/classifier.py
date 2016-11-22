0f911e482451f0fcf5103ade4764151b5d41d580
student777 <darkdakku@gmail.com>
2016-11-22 20:28:47 +0900

    Update classify_url: slash issue when created, Add comment

--- MemoSquare/classifier.py
+++ MemoSquare/classifier.py
@@ -7,17 +7,30 @@ def classify_url(url):
     # Ignore parameters like '#', only treats proper URI data
     # Treat same thing as same, different things as different
 
-    # If exists, get page. Otherwise create page
+    # Server doesn't know that if url in DB ends with slash or not, so search both
+    if url.endswith('/'):
+        url_trans = url[:-1]
+    else:
+        url_trans = url + '/'
+
+    # If exists('OR' search), get page. Otherwise create page
+    # By the way, will page.url stored in DB ends with slash or not? It depends on the request which first came
+    # If first request's url ends with slash, that url will be stored in DB and never change although next request's url
+    # does not ends with slash..
+    
     try:
         page = Page.objects.get(url=url)
     except Page.DoesNotExist:
-        page = Page.objects.create(url=url)
+        try:
+            page = Page.objects.get(url=url_trans)
+        except Page.DoesNotExist:
+            page = Page.objects.create(url=url)
     return page
 
 
 # find memo list of specific url
 def find_memo(url, request):
-    # Server don't know that if url in DB ends with slash or not, so execute 'OR' query
+    # Server doesn't know that if url in DB ends with slash or not, so search both
     if url.endswith('/'):
         url_trans = url[:-1]
     else:
