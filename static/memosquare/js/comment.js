375fba1296166042b616e8dc77e086b87042558d
student777 <darkdakku@gmail.com>
2017-01-18 14:01:04 +0900

    Add comment create/update/delete UI

--- static/memosquare/js/comment.js
+++ static/memosquare/js/comment.js
@@ -1,9 +1,9 @@
 function like_comment(pk) {
     $.ajax({
-        'url': '/comment/' + pk + '/like/',
-        "method": "POST",
-        'data': {"csrfmiddlewaretoken": csrf_token},
-        "success": function (response) {
+        url: '/comment/' + pk + '/like/',
+        method: "POST",
+        data: {csrfmiddlewaretoken: csrf_token},
+        success: function (response) {
             console.log(response)
         }
     })
@@ -12,15 +12,60 @@ function like_comment(pk) {
 function dislike_comment(pk) {
     $.ajaxSetup({
         beforeSend: function (xhr, settings) {
-            xhr.setRequestHeader("X-CSRFToken", csrf_token);
+            xhr.setRequestHeader('X-CSRFToken', csrf_token);
         }
     });
     $.ajax({
-        'url': '/comment/' + pk + '/like/',
-        "method": "DELETE",
-        'data': {"csrfmiddlewaretoken": csrf_token},
-        "success": function (response) {
+        url: '/comment/' + pk + '/like/',
+        method: "DELETE",
+        data: {csrfmiddlewaretoken: csrf_token},
+        success: function (response) {
             console.log(response)
         }
     })
 }
+
+function add_comment(pk){
+    var content = $('input#comment').val();
+    $.ajax({
+        url: '/comment/',
+        method: "POST",
+        data: {csrfmiddlewaretoken: csrf_token, memo: pk, content: content},
+        success: function (response) {
+            $('#comment_list').append(response.content);
+            console.log(response)
+        }
+    })
+}
+
+function edit_comment(pk){
+    var content = prompt("sometext","defaultText");
+    $.ajax({
+        url: '/comment/' + pk + '/',
+        method: "POST",
+        data: {csrfmiddlewaretoken: csrf_token, content: content},
+        success: function (response) {
+            $('#comment' + pk + '> td').eq(1).text(response.content);
+            console.log(response)
+        }
+    })
+}
+
+
+function delete_comment(pk){
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            xhr.setRequestHeader("X-CSRFToken", csrf_token);
+        }
+    });
+    $.ajax({
+        url: '/comment/' + pk + '/',
+        method: "DELETE",
+        success: function (response) {
+            $('#comment'+pk).remove();
+            console.log(response);
+        }
+    })
+}
+
+
